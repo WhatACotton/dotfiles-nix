6abdcaa62f2c4424ccb34490aaaea04da80e7ea7
whatacotton <whatacotton@gmail.com>
2024-10-25 17:19:33 +0900

    add zsh plugin

--- home/config/zsh/zsh.nix
+++ home/config/zsh/zsh.nix
@@ -7,6 +7,26 @@
     enableCompletion = true;
     plugins = [
       {
+    # will source zsh-autosuggestions.plugin.zsh
+    name = "zsh-autosuggestions";
+    src = pkgs.fetchFromGitHub {
+      owner = "zsh-users";
+      repo = "zsh-autosuggestions";
+      rev = "v0.7.0";
+      sha256 = "1g3pij5qn2j7v7jjac2a63lxd97mcsgw6xq6k5p7835q9fjiid98";
+    };
+  }
+  {
+    name = "zsh-syntax-highlighting";
+    src = pkgs.fetchFromGitHub {
+      owner = "zsh-users";
+      repo = "zsh-syntax-highlighting";
+      rev = "0.8.0";
+      sha256 = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
+    };
+  }
+  
+      {
         name = "zsh-nix-shell";
         file = "nix-shell.plugin.zsh";
         src = pkgs.fetchFromGitHub {
@@ -38,6 +58,7 @@
       "ns" = "firefox https://search.nixos.org";
       "gc" = "nix-collect-garbage";
       "t" = "typst compile";
+      "cd"="z";
     };
     # .zshrc
     initExtra = ''
